7477cabc3f4d43321744be6703e35f40440ac15e
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2024-01-11 11:08:15 +0530

    signIn screen

---
+++ app/src/main/java/com/example/blogapp/SigninAndRegisterationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.blogapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninAndRegisterationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_and_registeration)
+    }
+}
